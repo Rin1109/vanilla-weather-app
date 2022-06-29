1190fa6fc94b750db0369d94bc0f6b8efa3bda7c
Iryna <gresrin@gmail.com>
2022-06-29 23:29:19 +0200

    Added the search engine

--- src/app.js
+++ src/app.js
@@ -52,15 +52,26 @@ function displayTemperature(response) {
    iconElement.setAttribute(
       "alt",
       response.data.weather[0].description
-   );
+   );  
 
 
 
-   
+
 }
 
-let apiKey = ("2ef93a39c16392eacc33b8d28a8db82d");
-let city = "Hurghada";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+   let apiKey = ("2ef93a39c16392eacc33b8d28a8db82d");
+   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+   axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+   event.preventDefault();
+   let cityInputElement = document.querySelector("#city-input");
+   search(cityInputElement.value);  
+}
+
+search("Kyiv");
+
+let form = document.querySelector(".search-form");
+form.addEventListener('submit', handleSubmit);
