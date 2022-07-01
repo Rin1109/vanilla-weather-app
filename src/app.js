84da0724963f3b0fc45228208d05661b4cd25478
Iryna <gresrin@gmail.com>
2022-07-01 23:12:20 +0200

    Added JS Template

--- src/app.js
+++ src/app.js
@@ -24,6 +24,28 @@ function formatDate(timestamp) {
 
 }
 
+function displayForecast() {
+   let forecastElement = document.querySelector("#forecast");
+
+   let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tues"];
+
+   let forecastHTML = `<div class="row">`;
+   days.forEach(function (day) {
+      forecastHTML = forecastHTML + `
+      <div class="col-2">
+         <div class="weather-forecast-date">${day}</div>
+         <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="Sunny" width="36">
+         <div class="weather-forecast-tempereture">
+            <span class="weather-forecast-tempereture-max">18°</span>
+            <span class="weather-forecast-tempereture-min">12°</span>
+         </div>
+      </div>`;
+   });
+   forecastHTML = forecastHTML + `</div>`;
+   forecastElement.innerHTML = forecastHTML;
+
+}
+
 function displayTemperature(response) {
    let currentTemp = document.querySelector('#current-temp');
    let currentCity = document.querySelector('#city');
@@ -112,3 +134,5 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Kyiv");
+
+displayForecast();
